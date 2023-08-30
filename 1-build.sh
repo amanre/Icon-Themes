8ce30043288873273662529b73b27bd8ebc6f32a
amanre <amanre556@gmail.com>
2023-08-30 23:24:17 +0200

    update

--- 1-build.sh
+++ 1-build.sh
@@ -5,11 +5,11 @@ destination="/home/amanre/custom_repo/x86_64/"
 
 			makepkg -f
 
-			mv gtk-themes-git*pkg.tar.zst $destination
+			mv icon-themes-git*pkg.tar.zst $destination
 
 			rm -r pkg
 			rm -r src
-			rm -rf gtk-themes*
+			rm -rf icon-themes*
 			echo "#############################################################################################"
 			echo "################  "$(basename `pwd`)" done"
 			echo "#############################################################################################"
